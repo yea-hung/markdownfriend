b0d4e46e457c109a143a42f57ef67ee1d002ac66
yea-hung <7434605+yea-hung@users.noreply.github.com>
2026-03-30 15:59:39 -0700

    defined files

---
+++ R/read_markdown.r
@@ -0,0 +1,34 @@
+#' `read_markdown()`
+#'
+#' @param markdown a Markdown table
+#' @param lower_header an optional argument controlling whether or not the header should be lowercase 
+#'
+#' @returns a data.frame
+#' @export
+#'
+#' @examples
+#' markdown<-"
+#' fruit| price
+#' -----|-----:
+#' apple|2.05
+#' pear|1.37
+#' orange|3.09
+#' "
+#' read_markdown(markdown)
+#' @importFrom stats setNames
+#' @importFrom utils read.delim
+read_markdown<-function(markdown,lower_header=TRUE){
+  
+  read.delim(text=markdown,sep='|',strip.white=TRUE,skip=2) |>
+    (\(x) x[,2:(ncol(x)-1)])() |>
+    setNames(
+      readLines(textConnection(markdown),n=3) |>
+        (\(x) x[x!=''][1])() |>
+        (\(x) gsub('^\\s*\\|','',x))() |>
+        (\(x) gsub('\\|\\s*$','',x))() |>
+        (\(x) strsplit(x,split='|',fixed=TRUE)[[1]])() |>
+        trimws() |>
+        (\(x) if(lower_header){ tolower(x) } else {x} )()
+    )
+  
+}
